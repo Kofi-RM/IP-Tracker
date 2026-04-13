c46c167d689c1b8059b80c9ba5018e1ad85e95f3
Kofi-RM <kofireevesmiller@gmail.com>
2026-04-13 02:15:20 -0400

    Added ip searching

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ const ip = document.getElementById("ip")
 const locat = document.getElementById("location")
 const timezone = document.getElementById("timezone")
 const isp = document.getElementById("isp")
+const submit = document.getElementById("submit-btn");
+const ipBar = document.getElementById("ip-bar")
+
+let lat;
+let long;
 
 let ipInfo;
 let locationInfo;
@@ -17,7 +22,7 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 async function getIp() {
 
-    let response = await fetch("https://geo.ipify.org/api/v2/country?apiKey=at_iQNiAqmMttP2ybQTj2MVnSvCwUDrA");
+    let response = await fetch("https://geo.ipify.org/api/v2/country,city?apiKey=at_iQNiAqmMttP2ybQTj2MVnSvCwUDrA");
 
 let results = await response.json();
 
@@ -33,9 +38,38 @@ async function init() {
 
     console.log(results.location)
      ip.textContent = results.ip;
-    locat.textContent = results.location?.region + " " + results.location.country;
+    locat.textContent = results.location?.city + ", " + results.location.region + " " + results.location.postalCode;
     timezone.textContent = results.location?.timezone + " UTC";
-    isp.textContent = results.isp.split;
+    isp.textContent = results.isp;
+    lat = results.location.lat;
+    long = results.location.lng;
+
+    
+map.setView([lat, long],13)
+L.marker([lat, long]).addTo(map);
 }
 
-// init();
+async function searchIp(searchIp) {
+     let response = await fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=at_iQNiAqmMttP2ybQTj2MVnSvCwUDrA&ipAddress=${searchIp}`);
+    let results = await response.json();
+
+    console.log(results);
+       ip.textContent = results.ip;
+    locat.textContent = results.location?.city + ", " + results.location.region + " " + results.location.postalCode;
+    timezone.textContent = results.location?.timezone + " UTC";
+    isp.textContent = results.isp;
+    lat = results.location.lat;
+    long = results.location.lng;
+
+    
+map.setView([lat, long],13)
+L.marker([lat, long]).addTo(map);
+    }
+
+    submit.addEventListener("click", () => {
+        console.log(ipBar.value)
+        const searchValue = ipBar.value;
+        searchIp(searchValue);
+    })
+init();
+
